2e059f87f94be1e24e5b073e3163f81feab27e24
RenatoFernandoSilvaGoncalves <renatogoncalves@unoeste.edu.br>
2024-10-17 22:58:16 -0300

    Adicionando novas funcionalidades (deposito, contar) no aplicativo web.

--- index.js
+++ index.js
@@ -23,6 +23,87 @@ function paginaInicial(requisicao, resposta) {
              //   |
 app.get("/", paginaInicial);  //http://localhost:3000/ <== é a raiz da aplicação
 
+function mostrarSobre(requisicao, resposta) {
+    resposta.write(`<html>`)
+    resposta.write(`<head>
+                        <meta charset="UTF-8">
+                        <title>Sobre</title>
+                    </head>
+                    <body>`);
+    resposta.write(`<h1>Sobre nós</h1> `);
+    resposta.write(`<p>Somos um equipe de desenvolvimento iniciante.</p>`);
+    resposta.write(`<p>Estamos aprendendo a programar em JavaScript para oferecer aos nossos clientes as melhores soluções em aplicações para a Internet.</p>`);
+    resposta.write(`<p>Envie uma doação para o nosso e-mail: 3t6fY@example.com</p>`);
+    resposta.write(`<p>Adote um programador.</p>`);
+    resposta.write(`</body>`)
+    resposta.write(`</html>`)
+    resposta.end();
+}
+
+app.get("/sobre", mostrarSobre);
+
+
+//implementar uma rota que seja capaz de receber parâmetros na URL
+//a requisição deverá informar o valor a ser depositado por meio da url
+//exemplo http://localhost:3000/depositar?valor=10
+function depositar(requisicao, resposta){
+    const valor = requisicao.query.valor;
+    if (valor) {
+        resposta.write(`<html>
+                        <head>
+                           <meta charset="UTF-8">
+                           <title>Depósito realizado!</title>
+                        </head>
+                        <body>
+                            <h1>Depósito realizado!</h1> 
+                            <h1>Valor Depositado: R$ ${valor}</h1> 
+                            <h1>Obrigado!</h1>
+                        </body>
+                        </html>`);
+        resposta.end();
+    }
+    else{
+        resposta.write("<html>");
+        resposta.write("<head>");
+        resposta.write("<meta charset='UTF-8'>");
+        resposta.write("</head>");
+        resposta.write("<body>");
+        resposta.write("<h1>É necessário informar o valor a ser depositado!<h1>");
+        resposta.write("Exemplo: http://localhost:3000/depositar?valor=10");
+        resposta.write("</body>");
+        resposta.write("</html>");
+        resposta.end();
+    }
+}
+
+app.get("/depositar", depositar);
+
+
+function contar(requisicao, resposta){
+// criar uma página html (resposta) exibindo a faixa de valores
+//dos parâmetros inicio e fim informados pelo usuário.
+    const inicio = parseInt(requisicao.query.inicio);
+    const fim = parseInt(requisicao.query.fim);
+
+    if (inicio > 0 && fim > 0 && inicio < fim){
+        resposta.write("<p>Contando...</p>");
+        resposta.write("<ul>");
+        for (let i = inicio; i <= fim; i++){
+            resposta.write(`<li>${i}</li>`);
+        }
+        resposta.write("</ul>");
+        resposta.end();
+    }
+    else{
+        resposta.write("<p>Informe corretamente o inicio e o fim.</p>");
+        resposta.write("<p>Exemplo: http://localhost:3000/contar?inicio=1&fim=10</p>");
+    }
+
+
+}
+
+app.get("/contar", contar);
+
 app.listen(porta, host, () => {    //arrow function ou ainda função de seta
     console.log("Servidor em execução http://" + host + ":" + porta);
 });
